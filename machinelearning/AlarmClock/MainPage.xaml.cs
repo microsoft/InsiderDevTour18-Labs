8a2704f43a27006559da313c218f5ffac1482849
cbarkerms <cbarker@microsoft.com>
2018-07-11 07:21:43 -0700

    added mit license headers

--- machinelearning/AlarmClock/MainPage.xaml.cs
+++ machinelearning/AlarmClock/MainPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.ProjectOxford.Face;
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.ProjectOxford.Face;
 using Microsoft.ProjectOxford.Face.Contract;
 using System;
 using System.Collections.Generic;
